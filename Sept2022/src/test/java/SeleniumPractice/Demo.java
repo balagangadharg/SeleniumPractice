04dbca939c066a763cf1bc4c813b083663a84b14
balagangadharg <51108944+balagangadharg@users.noreply.github.com>
2022-11-14 13:21:17 +0530

    Update Demo.java
    Updated

--- Sept2022/src/test/java/SeleniumPractice/Demo.java
+++ Sept2022/src/test/java/SeleniumPractice/Demo.java
@@ -3,8 +3,14 @@ package SeleniumPractice;
 public class Demo {
 
 	public static void main(String[] args) {
-
-
+		
+		WebDriverManager.chromedriver().setupt();
+		WebDriver driver = new  ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.google.com/");
+		driver.getUrl();
+		driver.getTitle();
 		System.out.println("Github Testing!");
 		System.out.println("Selenium Testing!");
 		System.out.println("Pull to Msg!");
