d9b3973d5df40c9ec6c4c18949c548c33fc4ec4f
Gangadhara_g <Gangadhara_g@XOR-BSA-39.India.XoriantCorp.com>
2022-11-14 17:48:27 +0530

    Demo is Added.

--- Sept2022/src/test/java/SeleniumPractice/Demo.java
+++ Sept2022/src/test/java/SeleniumPractice/Demo.java
@@ -1,21 +1,43 @@
 package SeleniumPractice;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Demo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.saucedemo.com/");
 		driver.manage().window().maximize();
 
-		driver.get("https://www.google.com/");
-		System.out.println(driver.getCurrentUrl());
-		System.out.println(driver.getTitle());
+		WebElement user = driver.findElement(By.id("user-name"));
+		user.sendKeys("problem_user");
+		Thread.sleep(3000);
+
+		WebElement pwd = driver.findElement(By.id("password"));
+		pwd.sendKeys("secret_sauce");
+		Thread.sleep(3000);
+
+		WebElement login = driver.findElement(By.id("login-button"));
+		login.click();
+		Thread.sleep(3000);
+
+		WebElement menu = driver.findElement(By.xpath("//button[text()='Open Menu']"));
+		menu.click();
+		Thread.sleep(3000);
+
+		WebElement logout = driver.findElement(By.xpath("//a[text()='Logout']"));
+		logout.click();
+		
+		driver.close();
+
 	}
 
 }
