69517cfff6df12adcea286ba70d8cde84bc1d8e3
Gangadhara_g <Gangadhara_g@XOR-BSA-39.India.XoriantCorp.com>
2022-11-14 13:25:05 +0530

    Updated the Demo.

--- Sept2022/src/test/java/SeleniumPractice/Demo.java
+++ Sept2022/src/test/java/SeleniumPractice/Demo.java
@@ -1,20 +1,21 @@
 package SeleniumPractice;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Demo {
 
 	public static void main(String[] args) {
-		
-		WebDriverManager.chromedriver().setupt();
-		WebDriver driver = new  ChromeDriver();
-		driver.manage().window().maximize();
-		
-		driver.get("https://www.google.com/");
-		driver.getUrl();
-		driver.getTitle();
-		System.out.println("Github Testing!");
-		System.out.println("Selenium Testing!");
-		System.out.println("Pull to Msg!");
 
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+
+		driver.get("https://www.google.com/");
+		System.out.println(driver.getCurrentUrl());
+		System.out.println(driver.getTitle());
 	}
 
 }
